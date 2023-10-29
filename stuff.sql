6fc6581db3284d878390bc1791e035a110690dec
Franek Suszko <franeksu@gmail.com>
2023-10-29 23:00:40 +0100

    script update

--- stuff.sql
+++ stuff.sql
@@ -1,199 +1,151 @@
--- Tabela PojazdUdogodnienie
-CREATE TABLE PojazdUdogodnienie (
-    PojazdUdogodnienieID INT PRIMARY KEY,
-    PojazdID INT,
-    UdogodnienieID INT
+-- VehicleType Table
+CREATE TABLE VehicleType (
+    VehicleTypeID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL UNIQUE,
 );
 
--- Tabela PrzewoznikPojazd
-CREATE TABLE PrzewoznikPojazd (
-    PrzewoznikPojazdID INT PRIMARY KEY,
-    PrzewoznikID INT,
-    PojazdID INT
+-- UserType Table
+CREATE TABLE UserType (
+    UserTypeID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL UNIQUE,
 );
 
--- Tabela Przewoznicy
-CREATE TABLE Przewoznicy (
-    PrzewoznikID INT PRIMARY KEY,
-    Nazwa VARCHAR(255)
+-- Carriers Table
+CREATE TABLE Carriers (
+    CarrierID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL UNIQUE,
 );
 
--- Tabela Linia
-CREATE TABLE Linia (
-    LiniaID INT PRIMARY KEY,
-    NumerLinii INT
+-- Line Table
+CREATE TABLE Line (
+    LineID SERIAL PRIMARY KEY,
+    LineNumber INT
+
+    CHECK (LineNumber >= 0),
 );
 
--- Tabela Uzytkownik
-CREATE TABLE Uzytkownik (
-    UserID INT PRIMARY KEY,
-    Username VARCHAR(255),
-    Password VARCHAR(255),
-    Email VARCHAR(255),
-    UserType VARCHAR(255),
-    CreatedAt TIMESTAMP
+-- User Table
+CREATE TABLE "User" (
+    UserID SERIAL PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    CreatedAt TIMESTAMP NOT NULL DEFAULT NOW(),
+    UserTypeID INT REFERENCES UserType(UserTypeID) DEFAULT 1 ON DELETE RESTRICT,
+
+
+    CHECK (Email LIKE '%@%.%'),
 );
 
--- Tabela RozkladJazdy
-CREATE TABLE RozkladJazdy (
-    RozkladID INT PRIMARY KEY,
-    LiniaID INT,
-    PrzystanekID INT,
-    GodzinaOdjazdu TIMESTAMP,
-    GodzinaPrzyjazdu TIMESTAMP
+-- Schedule Table
+CREATE TABLE Schedule (
+    ScheduleID SERIAL PRIMARY KEY,
+    LineID INT REFERENCES Line(LineID) ON DELETE CASCADE,
+    StopID INT NOT NULL UNIQUE REFERENCES Stop(StopID) ON DELETE RESTRICT ON UPDATE CASCADE,
+    DepartureTime TIMESTAMP NOT NULL,
+    ArrivalTime TIMESTAMP NOT NULL,
+
+    CHECK (DepartureTime > ArrivalTime)
 );
 
--- Tabela Przystanek
-CREATE TABLE Przystanek (
-    PrzystanekID INT PRIMARY KEY,
-    Nazwa VARCHAR(255),
-    TrasaID INT
+-- Stop Table
+CREATE TABLE Stop (
+    StopID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL UNIQUE,
 );
 
--- Tabela Pojazd
-CREATE TABLE Pojazd (
-    PojazdID INT PRIMARY KEY,
-    NumerRejestracyjny VARCHAR(255),
-    TypPojazdu VARCHAR(255)
+-- Vehicle Table
+CREATE TABLE Vehicle (
+    VehicleID SERIAL PRIMARY KEY,
+    RegistrationNumber VARCHAR(255) NOT NULL UNIQUE,
+    VehicleTypeID INT NOT NULL REFERENCES VehicleType(VehicleTypeID) ON DELETE RESTRICT,
 );
 
--- Tabela Udogodnienia
-CREATE TABLE Udogodnienia (
-    UdogodnienieID INT PRIMARY KEY,
-    Nazwa VARCHAR(255),
-    Opis VARCHAR(255)
+-- Amenities Table
+CREATE TABLE Amenities (
+    AmenityID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL UNIQUE,
+    Description VARCHAR(255)
 );
 
--- Tabela Podroz
-CREATE TABLE Podroz (
-    PodrozID INT PRIMARY KEY,
-    UzytkownikID INT,
-    BiletID INT,
-    RozkladID INT
+-- Journey Table
+CREATE TABLE Journey (
+    JourneyID SERIAL PRIMARY KEY,
+    UserID INT REFERENCES "User"(UserID) ON DELETE CASCADE ON UPDATE RESTRICT,
+    TicketID INT REFERENCES Ticket(TicketID) ON DELETE NO ACTION ON UPDATE RESTRICT,
+    ScheduleID INT REFERENCES Schedule(ScheduleID)
 );
 
--- Tabela Kurs
-CREATE TABLE Kurs (
-    KursID INT PRIMARY KEY,
-    NumerKursu INT
+-- Course Table
+CREATE TABLE Course (
+    CourseID SERIAL PRIMARY KEY,
+    CourseNumber INT NOT NULL UNIQUE,
+
+    CHECK (CourseNumber > 0),
 );
 
--- Tabela Trasa
-CREATE TABLE Trasa (
-    TrasaID INT PRIMARY KEY,
-    Nazwa VARCHAR(255),
-    Opis VARCHAR(255)
+-- Route Table
+CREATE TABLE Route (
+    RouteID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL UNIQUE,
+    Description VARCHAR(255)
 );
 
--- Tabela Utrudnienia
-CREATE TABLE Utrudnienia (
-    UtrudnienieID INT PRIMARY KEY,
-    Opis VARCHAR(255)
+-- Obstacles Table
+CREATE TABLE Obstacles (
+    ObstacleID SERIAL PRIMARY KEY,
+    Description VARCHAR(255),
 );
 
--- Tabela Bilet
-CREATE TABLE Bilet (
-    BiletID INT PRIMARY KEY,
-    Cena DECIMAL(8, 2),
-    LiniaID INT
+-- Ticket Table
+CREATE TABLE Ticket (
+    TicketID SERIAL PRIMARY KEY,
+    Price DECIMAL(8, 2),
+    LineID INT REFERENCES Line(LineID),
+
+    CHECK (Price >= 0)
 );
 
--- Tabela Przesiadka
-CREATE TABLE Przesiadka (
-    PrzesiadkaID INT PRIMARY KEY,
-    TrasaID INT,
-    PrzystanekID INT,
-    LiniaID INT,
-    PodrozID INT
+-- Transfer Table
+CREATE TABLE Transfer (
+    TransferID SERIAL PRIMARY KEY,
+    RouteID INT REFERENCES Route(RouteID),
+    StopID INT REFERENCES Stop(StopID),
+    LineID INT REFERENCES Line(LineID),
+    JourneyID INT REFERENCES Journey(JourneyID)
 );
 
--- Tabela TrasyLinii
-CREATE TABLE TrasyLinii (
-    TrasaLiniiID INT PRIMARY KEY,
-    TrasaID INT,
-    LiniaID INT
+-- VehicleAmenity Table
+CREATE TABLE VehicleAmenity (
+    VehicleAmenityID SERIAL PRIMARY KEY,
+    VehicleID INT REFERENCES Vehicle(VehicleID) ON DELETE CASCADE,
+    AmenityID INT REFERENCES Amenities(AmenityID) ON DELETE CASCADE,
 );
 
--- Tabela PrzystankiTrasy
-CREATE TABLE PrzystankiTrasy (
-    PrzystanekTrasyID INT PRIMARY KEY,
-    PrzystanekID INT,
-    TrasaID INT
+-- CarrierVehicle Table
+CREATE TABLE CarrierVehicle (
+    CarrierVehicleID SERIAL PRIMARY KEY,
+    CarrierID INT REFERENCES Carriers(CarrierID) ON DELETE RESTRICT ON UPDATE CASCADE,
+    VehicleID INT REFERENCES Vehicle(VehicleID) ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
--- Tabela UtrudnieniaTrasy
-CREATE TABLE UtrudnieniaTrasy (
-    UtrudnienieTrasyID INT PRIMARY KEY,
-    UtrudnienieID INT,
-    TrasaID INT
+-- LineRoutes Table
+CREATE TABLE LineRoutes (
+    LineRouteID SERIAL PRIMARY KEY,
+    RouteID INT REFERENCES Route(RouteID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    LineID INT REFERENCES Line(LineID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
--- Definicje kluczy obcych
-ALTER TABLE Przesiadka
-ADD FOREIGN KEY (TrasaID) REFERENCES Trasa(TrasaID);
+-- RouteStops Table
+CREATE TABLE RouteStops (
+    RouteStopID SERIAL PRIMARY KEY,
+    StopID INT REFERENCES Stop(StopID) ON DELETE RESTRICT ON UPDATE CASCADE,
+    RouteID INT REFERENCES Route(RouteID) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-ALTER TABLE Przesiadka
-ADD FOREIGN KEY (PrzystanekID) REFERENCES Przystanek(PrzystanekID);
-
-ALTER TABLE Przesiadka
-ADD FOREIGN KEY (LiniaID) REFERENCES Linia(LiniaID);
-
-ALTER TABLE Przesiadka
-ADD FOREIGN KEY (PodrozID) REFERENCES Podroz(PodrozID);
-
-ALTER TABLE PojazdUdogodnienie
-ADD FOREIGN KEY (PojazdID) REFERENCES Pojazd(PojazdID);
-
-ALTER TABLE PrzewoznikPojazd
-ADD FOREIGN KEY (PrzewoznikID) REFERENCES Przewoznicy(PrzewoznikID);
-
-ALTER TABLE Przystanek
-ADD FOREIGN KEY (TrasaID) REFERENCES Trasa(TrasaID);
-
-ALTER TABLE Bilet
-ADD FOREIGN KEY (LiniaID) REFERENCES Linia(LiniaID);
-
-ALTER TABLE RozkladJazdy
-ADD FOREIGN KEY (PrzystanekID) REFERENCES Przystanek(PrzystanekID);
-
-ALTER TABLE PojazdUdogodnienie
-ADD FOREIGN KEY (UdogodnienieID) REFERENCES Udogodnienia(UdogodnienieID);
-
-ALTER TABLE PrzewoznikPojazd
-ADD FOREIGN KEY (PojazdID) REFERENCES Pojazd(PojazdID);
-
-ALTER TABLE Trasa
-ADD FOREIGN KEY (TrasaID) REFERENCES Bilet(BiletID);
-
-ALTER TABLE Podroz
-ADD FOREIGN KEY (UzytkownikID) REFERENCES Uzytkownik(UserID);
-
-ALTER TABLE Kurs
-ADD FOREIGN KEY (KursID) REFERENCES Pojazd(PojazdID);
-
-ALTER TABLE Kurs
-ADD FOREIGN KEY (KursID) REFERENCES Linia(LiniaID);
-
-ALTER TABLE RozkladJazdy
-ADD FOREIGN KEY (RozkladID) REFERENCES Linia(LiniaID);
-
-ALTER TABLE Trasa
-ADD FOREIGN KEY (TrasaID) REFERENCES Linia(LiniaID);
-
-ALTER TABLE Przystanek
-ADD FOREIGN KEY (PrzystanekID) REFERENCES PrzystankiTrasy(PrzystanekID);
-
-ALTER TABLE Trasa
-ADD FOREIGN KEY (TrasaID) REFERENCES PrzystankiTrasy(TrasaID);
-
-ALTER TABLE Linia
-ADD FOREIGN KEY (LiniaID) REFERENCES TrasyLinii(LiniaID);
-
-ALTER TABLE Trasa
-ADD FOREIGN KEY (TrasaID) REFERENCES TrasyLinii(TrasaID);
-
-ALTER TABLE Utrudnienia
-ADD FOREIGN KEY (UtrudnienieID) REFERENCES UtrudnieniaTrasy(UtrudnienieID);
-
-ALTER TABLE Trasa
-ADD FOREIGN KEY (TrasaID) REFERENCES UtrudnieniaTrasy(TrasaID);
+-- RouteObstacles Table
+CREATE TABLE RouteObstacles (
+    RouteObstacleID SERIAL PRIMARY KEY,
+    ObstacleID INT REFERENCES Obstacles(ObstacleID) ON UPDATE CASCADE ON DELETE CASCADE,
+    RouteID INT REFERENCES Route(RouteID) ON UPDATE CASCADE ON DELETE CASCADE
+);
